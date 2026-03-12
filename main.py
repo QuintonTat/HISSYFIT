a5189dde7737411ff1d22fce89e45f37f94ced17
QuintonTat <Quinton.tat@education.nsw.gov.au>
2026-03-12 23:17:01 +1100

    background added

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import pygame, random
 
 pygame.init() # initalises pygame
 
-width, height = 1000, 500 # establishes width and height variables
+width, height = 950, 500 # establishes width and height variables
 screen = pygame.display.set_mode((width, height)) # creates the screen
 
-while True:
-    for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                raise SystemExit
+surface = pygame.image.load("background.png")
 
+while True:
+    # makes the screen stay up without it it wont work
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            raise SystemExit
+    
+    # game logic here
+    screen.blit(surface, (0,0))
+
+    pygame.display.update()
 
 
     
